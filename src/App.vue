fe346214f687da3cd6067473376f654ff037cff8
Blaz <blazyy@gmail.com>
2021-12-02 15:20:35 +0100

    added @click; startGame() created; minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <h2>Reaction Timer</h2>
+  <button @click="startGame" :disabled="isPlaying">Play</button>
+
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
+import Block from "./components/Block.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    Block,
+  },
+  data() {
+    return {
+      delay: null,
+      isPlaying: false,
+    };
+  },
+  methods: {
+    startGame() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+    },
+  },
 };
 </script>
 
@@ -15,7 +33,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
